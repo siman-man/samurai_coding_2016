c671926dcf98ba1e14440fc7977b44a6c0898aab
siman-man <k128585@ie.u-ryukyu.ac.jp>
2015-11-21 23:11:31 +0900

    first commit

---
+++ siman.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <limits.h>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <set>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+typedef long long ll;
+
+int main(){
+  return 0;
+}
